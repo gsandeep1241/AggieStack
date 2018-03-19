1d5a902729a360300da7e01d2e7e95c3c8159c28
Sandeep Gottimukkala <sgottimukkal@tamu.edu>
2018-03-18 21:52:17 -0500

    Setup handlers

--- P1/src/main.py
+++ P1/src/main.py
@@ -5,3 +5,42 @@ while True:
     if cmd_parts[0] == "quit":
         print("Goodbye!")
         break;
+        
+        
+def handle_config(cmd_parts):
+    if(len(cmd_parts) != 4):
+        #error
+        print("Goodbye!")
+    if(cmd_parts[2] == "--hardware"):
+        #handle1
+        print("Handle 1!")
+    elif(cmd_parts[2] == "--images"):
+        #handle2
+        print("Handle 2!")
+    elif(cmd_parts[2] == "--flavors"):
+        #handle3
+        print("Handle 3!")
+    else:
+        #handle_error
+        print("Handle Error!")
+        
+
+def handle_display(cmd_parts):
+    if(len(cmd_parts) != 3):
+        #error
+        print("Goodbye!")
+    if(cmd_parts[2] == "hardware"):
+        #handle1
+        print("Handle 1!")
+    elif(cmd_parts[2] == "images"):
+        #handle2
+        print("Handle 2!")
+    elif(cmd_parts[2] == "flavors"):
+        #handle3
+        print("Handle 3!")
+    elif(cmd_parts[2] == "all"):
+        #handle4
+        print("Handle 4!")
+    else:
+        #handle_error
+        print("Handle Error!")
