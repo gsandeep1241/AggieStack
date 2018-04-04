f8a5be7d66651b5f6483fbe006c82a87378b5782
Sandeep Gottimukkala <sgottimukkal@tamu.edu>
2018-04-04 02:21:13 -0500

    New classes for hardware and image

---
+++ P1/src/new_hardware.py
@@ -0,0 +1,8 @@
+class NewHardware:
+    def __init__(self, name, rack, ip, mem, num_disks, num_vcpus):
+        self.name = name
+        self.rack - rack
+        self.ip = ip
+        self.mem = mem
+        self.num_disks = num_disks
+        self.num_vcpus = num_vcpus
---
+++ P1/src/new_image.py
@@ -0,0 +1,5 @@
+class NewImage:
+    def __init__(self, name, size, path):
+        self.name = name
+        self.size = size
+        self.path = path
