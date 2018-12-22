72247ae4a50891dff52263b073383482db702974
Sandeep Gottimukkala <sandeep.gottimukkala@thoughtspot.com>
2018-12-22 06:04:20 +0530

    AggieStack

--- HW1/task1/constants.py
+++
@@ -1,4 +0,0 @@
-host = "34.233.78.56"
-db = "fitness_826006434"
-collection = "fitness_826006434"
-
--- HW1/task2/constants.py
+++
@@ -1,4 +0,0 @@
-host = "34.233.78.56"
-db = "message_board_826006434"
-collection = "message_board_826006434"
-
